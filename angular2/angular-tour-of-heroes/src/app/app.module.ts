b44050a2f4ed778c50d55c095408c30363920e63
Navin Pathak <np4navin@gmail.com>
2017-02-12 22:32:41 -0600

    Added HTTP calls

--- angular2/angular-tour-of-heroes/src/app/app.module.ts
+++ angular2/angular-tour-of-heroes/src/app/app.module.ts
@@ -12,22 +12,29 @@ import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 /** Services **/
 import { HeroService } from './hero.service';
+
+/** Imports for loading & configuring the in-memory web api **/
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
